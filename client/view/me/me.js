b6149b8f51e3d73312f731a5b8b43d7b88ba21a1
hi363138911 <liangya363138911@126.com>
2017-09-01 17:16:50 +0800

    增加账号系统

--- client/view/me/me.js
+++ client/view/me/me.js
@@ -1,3 +1,60 @@
-// Venues = 'fuck';
-// console.log(Venues);
-//
+//设置模版的值，可在模版上获取。
+Template.me.helpers({
+    title() {
+        return '我';
+    },
+})
+
+
+Template.register.events({
+    'submit form': function (event) {
+        event.preventDefault();
+
+        var registerData = {
+            email: event.target.registerEmail.value,
+            password: event.target.registerPassword.value
+        }
+
+        Accounts.createUser(registerData, function (error) {
+            if (Meteor.user()) {
+                console.log(Meteor.userId());
+            } else {
+                console.log("ERROR: " + error.reason);
+            }
+        });
+    }
+});
+
+Template.login.events({
+    'submit form': function (event) {
+        event.preventDefault();
+        var myEmail = event.target.loginEmail.value;
+        var myPassword = event.target.loginPassword.value;
+
+        Meteor.loginWithPassword(myEmail, myPassword, function (error) {
+            if (Meteor.user()) {
+                console.log(Meteor.userId());
+            } else {
+                console.log("ERROR: " + error.reason);
+            }
+        });
+    }
+});
+
+Template.success.events({
+    'click .logout': function (event) {
+        event.preventDefault();
+
+        Meteor.logout(function (error) {
+            if (error) {
+                console.log("ERROR: " + error.reason);
+            }
+        });
+    }
+});
+
+
+Template.success.onCreated(function () {
+    // console.log('----------', this.username);
+
+})
